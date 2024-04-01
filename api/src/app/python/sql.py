8a50913974975ce861425b2e34eef83b501cf9cc
HinanoOno <hinagon1231@icloud.com>
2024-04-02 03:16:14 +0900

    refactor: 各クラス内のメソッドを修正

--- api/src/app/python/sql.py
+++ api/src/app/python/sql.py
@@ -29,29 +29,6 @@ class DataAccessor:
         data = self.cursor.fetchall()
         return data
 
-    @staticmethod
-    def convert_ratings_data_to_dataframe(ratings_data: list) -> pd.DataFrame:
-        data_array = np.array(ratings_data)
-        columns = ["user_id", "shop_id", "rating"]
-        df = pd.DataFrame(data_array, columns=columns)
-        return df
-
-    @staticmethod
-    def dataframe_to_table(dataframe) -> pd.DataFrame:
-        return pd.pivot_table(
-            dataframe, index="user_id", columns="shop_id", values="rating"
-        )
-
-    def load_data(self) -> pd.DataFrame | None:
-        try:
-            ratings_data = self.fetch_ratings_data()
-            dataframe = self.convert_ratings_data_to_dataframe(ratings_data)
-            ratings_table = self.dataframe_to_table(dataframe)
-            return ratings_table
-        except Exception as e:
-            print(f"Error: {e}")
-        return None
-
     def delete_recommendations_from_db(self, user_id: int):
         sql = "DELETE FROM recommendations WHERE user_id = %s"
         val = (user_id,)
@@ -74,64 +51,66 @@ class DataAccessor:
             print(f"Error: {e}")
 
 
-class SimilarUsers:
-    def __init__(self):
-        self.similar_users_list = []
-        self.similarities = []
-        self.avg_ratings = []
+class RatingsManager:
+    def __init__(self, ratings_data):
+        self.ratings_table = self._load_data(ratings_data)
 
-    def add_similar_user(self, user_id: int, similarity: int, avg_rating: int):
-        self.similar_users_list.append(user_id)
-        self.similarities.append(similarity)
-        self.avg_ratings.append(avg_rating)
+    @staticmethod
+    def convert_ratings_data_to_dataframe(ratings_data: list) -> pd.DataFrame:
+        data_array = np.array(ratings_data)
+        columns = ["user_id", "shop_id", "rating"]
+        df = pd.DataFrame(data_array, columns=columns)
+        return df
+
+    @staticmethod
+    def dataframe_to_table(dataframe) -> pd.DataFrame:
+        return pd.pivot_table(
+            dataframe, index="user_id", columns="shop_id", values="rating"
+        )
+
+    def _load_data(self, ratings_data: list) -> pd.DataFrame | None:
+        try:
+            dataframe = self.convert_ratings_data_to_dataframe(ratings_data)
+            ratings_table = self.dataframe_to_table(dataframe)
+            return ratings_table
+        except Exception as e:
+            print(f"Error: {e}")
+        return None
+
+    def get_ratings_table(self) -> pd.DataFrame:
+        return self.ratings_table
 
 
-class Recommendee:
-    def __init__(self, user_id: int):
-        self.id = user_id
-        self.similar_users = SimilarUsers()
-
-
-class SimilarUsersFinder:
-    def __init__(self, ratings_table: pd.DataFrame, user_id: int):
+class UserSimilarity:
+    def __init__(self, ratings_table: pd.DataFrame):
         self.ratings_table = ratings_table
-        self.recommendee = Recommendee(user_id)
 
-    def find_rated_shops(self, candidate: int) -> tuple[np.ndarray, np.ndarray]:
-        recommendee_rated_shops = self.ratings_table.loc[
-            self.recommendee.id, :
-        ].to_numpy()
+    def find_common_shops(
+        self, recommendee: int, candidate: int
+    ) -> tuple[np.ndarray, np.ndarray]:
+        recommendee_rated_shops = self.ratings_table.loc[recommendee, :].to_numpy()
         candidate_rated_shops = self.ratings_table.loc[candidate, :].to_numpy()
-        return recommendee_rated_shops, candidate_rated_shops
 
-    def find_common_shops_ids(
-        self, recommendee_rated_shops: np.ndarray, candidate_rated_shops: np.ndarray
-    ) -> np.ndarray:
-        return ~np.isnan(recommendee_rated_shops) & ~np.isnan(candidate_rated_shops)
-
-    def find_common_shops(self, candidate: int) -> tuple[np.ndarray, np.ndarray]:
-        recommendee_rated_shops, candidate_rated_shops = self.find_rated_shops(
-            candidate
+        common_shops_ids = ~np.isnan(recommendee_rated_shops) & ~np.isnan(
+            candidate_rated_shops
         )
 
-        common_shops_ids = self.find_common_shops_ids(
-            recommendee_rated_shops, candidate_rated_shops
-        )
-
-        recommendee_rated_common_shops, candidate_rated_common_shops = (
-            recommendee_rated_shops[common_shops_ids],
-            candidate_rated_shops[common_shops_ids],
-        )
+        recommendee_rated_common_shops = recommendee_rated_shops[common_shops_ids]
+        candidate_rated_common_shops = candidate_rated_shops[common_shops_ids]
 
         return recommendee_rated_common_shops, candidate_rated_common_shops
 
-    def find_similar_users(self) -> tuple[list[int], list[float], list[float]]:
+    def find_similar_users(
+        self, recommendee: int
+    ) -> tuple[list[int], list[float], list[float]]:
+        similar_users = defaultdict(list)
+
         for candidate in self.ratings_table.index:
-            if self.recommendee.id == candidate:
+            if recommendee == candidate:
                 continue
 
             recommendee_rated_common_shops, candidate_rated_common_shops = (
-                self.find_common_shops(candidate)
+                self.find_common_shops(user_id, candidate)
             )
 
             if not recommendee_rated_common_shops.any():
@@ -143,20 +122,20 @@ class SimilarUsersFinder:
 
             if correlation_coefficent > 0:
                 avg_rating = np.mean(candidate_rated_common_shops)
-                self.recommendee.similar_users.add_similar_user(
-                    candidate, correlation_coefficent, avg_rating
-                )
+                similar_users["similar_users_list"].append(candidate)
+                similar_users["similarities"].append(correlation_coefficent)
+                similar_users["avg_ratings"].append(avg_rating)
 
         return (
-            self.recommendee.similar_users.similar_users_list,
-            self.recommendee.similar_users.similarities,
-            self.recommendee.similar_users.avg_ratings,
+            similar_users["similar_users_list"],
+            similar_users["similarities"],
+            similar_users["avg_ratings"],
         )
 
     def select_rated_similar_users(
-        self, shop_id: int
+        self, user_id: int, shop_id: int
     ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-        similar_users_list, similarities, avg_ratings = self.find_similar_users()
+        similar_users_list, similarities, avg_ratings = self.find_similar_users(user_id)
 
         similar_users_ratings = self.ratings_table.loc[
             similar_users_list, shop_id
@@ -172,9 +151,10 @@ class SimilarUsersFinder:
 
 
 class Recommender:
-    def __init__(self, ratings_table: pd.DataFrame, user_id: int):
-        self.similarUsersFinder = SimilarUsersFinder(ratings_table, user_id)
-        self.ratings_table = ratings_table
+    def __init__(self, ratings_manager, user_id: int):
+        self.user_id = user_id
+        self.ratings_table = ratings_manager.get_ratings_table()
+        self.user_similarity = UserSimilarity(self.ratings_table)
 
     @staticmethod
     def calculate_normalized_rating_difference(
@@ -203,7 +183,7 @@ class Recommender:
             similar_users_ratings,
             similar_users_similarities,
             similar_users_avg_ratings,
-        ) = self.similarUsersFinder.select_rated_similar_users(shop_id)
+        ) = self.user_similarity.select_rated_similar_users(self.user_id, shop_id)
 
         if not similar_users_ratings.any():
             return None
@@ -223,10 +203,7 @@ class Recommender:
         return item[1] if item[1] is not None else float("-inf")
 
     def recommend(self) -> list[int]:
-
-        user_avg_rating = self.ratings_table.loc[
-            self.similarUsersFinder.recommendee.id, :
-        ].mean()
+        user_avg_rating = self.ratings_table.loc[self.user_id, :].mean()
 
         predicted_ratings = {}
 
@@ -246,9 +223,10 @@ def main(user_id):
     try:
         connection, cursor = connect_to_db()
         dataAccessor = DataAccessor(cursor, connection)
-        ratings_table = dataAccessor.load_data()
+        ratings_table = dataAccessor.fetch_ratings_data()
 
-        recommend_shops = Recommender(ratings_table, user_id).recommend()
+        ratings_manager = RatingsManager(ratings_table)
+        recommend_shops = Recommender(ratings_manager, user_id).recommend()
 
         dataAccessor.insert_recommendations_to_db(user_id, recommend_shops)
         print(recommend_shops)
